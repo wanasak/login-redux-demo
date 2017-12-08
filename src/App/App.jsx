9c042213d98be984906cb2b1ddd053c593d7b61d
Wanasak <u510610433@gmail.com>
2017-12-08 17:39:37 +0700

    add app container

---
+++ src/App/App.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, Router } from 'react-router-dom';
+
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+import { history } from '../_helpers';
+import HomePage from '../HomePage/HomePage';
+import LoginPage from '../LoginPage/LoginPage';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+        const { dispatch } = this.props
+
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear())
+        })
+    }
+
+    render() {
+        const { alert } = this.props
+
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        { alert.message && 
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+
+                        <Router history={history}>
+                            <div>
+                                <PrivateRoute exact path="/" component={HomePage} />
+                                <Route path="/login" component={LoginPage} />
+                            </div>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    const { alert } = state
+
+    return { alert }
+}
+
+export default connect(mapStateToProps)(App);
